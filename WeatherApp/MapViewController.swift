08952674d07880ea008f43d68c676a2dcfd18c77
Varun Rathi <varunrathi28@gmail.com>
2017-12-12 23:10:37 +0530

    Initial Commit

---
+++ WeatherApp/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Varun Rathi on 12/12/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
